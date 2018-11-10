58deee8bbce3deedc71a0dd2304573cd864c56d4
rainmeter <rainmeter92@gmail.com>
2018-11-10 16:38:54 +0900

    NN 임포트 수정, MNIST 데이터 셋 학습결과 Accuracy

--- perceptron/MNIST/mnistTest.py
+++ perceptron/MNIST/mnistTest.py
@@ -1,21 +1,47 @@
+
+# 상위폴더 NN 임포트 시키는 방법임
 import sys, os
-sys.path.append(os.pardir)
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from NN import sigmoid,relu
+###########################
+
 import numpy as np
 from mnist import load_mnist
 from PIL import Image
+import pickle
 
-def img_show(img):
-    pil_img = Image.fromarray(np.uint8(img))
-    pil_img.show()
 
-(x_train, t_train), (x_test, t_test) =load_mnist(flatten=True, normalize=False)
+def get_data():
+    (x_train, t_train), (x_test, t_test) =load_mnist(flatten=True, \
+    normalize=True, one_hot_label=False)
+    return x_test, t_test
 
-img = x_train[2]
-label = t_train[2]
-print(label)
+def init_network():
+    with open('sample_weight.pkl','rb') as f:
+        network = pickle.load(f)
+    return network
 
-print(img.shape)
-img = img.reshape(28,28)
-print(img.shape)
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-img_show(img)
+    a1 = np.dot(x,W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = a3
+
+    return y
+
+x, t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt +=1
+
+print('Accuracy:'+ str(float(accuracy_cnt) / len(x) ))
